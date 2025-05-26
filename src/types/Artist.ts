dfd5a26afa6a77bb87120be5ef41f56dccf683ad
Prue Singh <pruesinghnagra@gmail.com>
2025-05-26 23:44:04 +1200

    fix shape of data

--- src/types/Artist.ts
+++ src/types/Artist.ts
@@ -1,25 +1,23 @@
 export interface Platforms {
-    platforms: {
-        spotify: {
-            followers: number;
-            monthyListeners: number;
-            playlistCount: number;
-        };
-        youtube: {
-            subscribers: number;
-            averageViews: number;
-        };
-        bandcamp: {
-            followers: number;
-            totalReleases: number;
-        };
+    spotify: {
+        followers: number;
+        monthlyListeners: number;
+        playlistCount: number;
+    };
+    youtube: {
+        subscribers: number;
+        averageViews: number;
+    };
+    bandcamp: {
+        followers: number;
+        totalReleases: number;
     };
 }
 
 export interface Artist {
     artistName: string;
     genre: string[];
-    region: string[];
+    region: string;
     foodType: string[];
     language: string[];
     platforms: Platforms;
