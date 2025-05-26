ab881b612d175eb08c56705cbc4686afd332409e
Prue Singh <pruesinghnagra@gmail.com>
2025-05-26 23:45:09 +1200

    add sort and filter - add styling

--- src/components/ArtistCard.tsx
+++ src/components/ArtistCard.tsx
@@ -7,27 +7,27 @@ interface Props {
 
 export default function ArtistCard({ artist }: Props) {
   return (
-    <div>
-      <h2>{artist.artistName}</h2>
-      <p>{artist.region}</p>
-      <p>
-        <strong>Genre:</strong>
+    <div className="border rounded-2xl shadow-md p-4">
+      <h2 className="text-xl font-bold mb-1">{artist.artistName}</h2>
+      <p className="text-sm text-gray-600">{artist.region}</p>
+      <p className="mt-2">
+        <strong>Genre: </strong>
         {artist.genre.join(', ')}
       </p>
       <p>
-        <strong>Food Type:</strong>
+        <strong>Food Type: </strong>
         {artist.foodType.join(', ')}
       </p>
       <p>
-        <strong>Language:</strong>
+        <strong>Language: </strong>
         {artist.language.join(', ')}
       </p>
       <p>
-        <strong>Active Since:</strong>
+        <strong>Active Since: </strong>
         {artist.activeSince}
       </p>
       <p>
-        <strong>Self Described Independent Artist:</strong>
+        <strong>Self Described Independent Artist: </strong>
         {artist.selfDescribedIndie ? 'yes' : 'no'}
       </p>
     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,55 @@
-import React from 'react';
+import { useMemo, useState } from 'react';
 import useArtists from '../hooks/useArtists';
 import ArtistCard from '../components/ArtistCard';
+import { Artist } from '../types/Artist';
 
 export default function Home() {
   const { artists, loading, error } = useArtists();
+  const [regionFilter, setRegionFilter] = useState<string>('all');
+  const [sortBy, setSortBy] = useState<string>('default');
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error}</p>;
+  const filteredAndSortedArtists = useMemo(() => {
+    if (loading || error || !artists) return [];
+
+    let result = [...artists];
+
+    if (regionFilter !== 'all') {
+      result = result.filter((artist) => artist.region === regionFilter);
+    }
+
+    if (sortBy === 'value') {
+      result.sort((a, b) => {
+        const aValue = a.platforms.spotify.monthlyListeners + a.platforms.youtube.averageViews;
+        const bValue = b.platforms.spotify.monthlyListeners + b.platforms.youtube.averageViews;
+        return bValue - aValue; // decending order
+      });
+    }
+
+    return result;
+  }, [artists, regionFilter, sortBy]);
 
   return (
-    <div>
-      {artists.map((artist, index) => (
-        <ArtistCard key={index} artist={artist} />
-      ))}
-    </div>
+    <>
+      <p>Current Region Filter: {regionFilter}</p>
+      <div className="flex gap-4 mb-6 items-center">
+        <select value={regionFilter} onChange={(e) => setRegionFilter(e.target.value)}>
+          <option value="all">All Regions</option>
+          <option value="Auckland">Auckland</option>
+          <option value="Wellington">Wellington</option>
+          <option value="Christchurch">Christchurch</option>
+          <option value="Dunedin">Dunedin</option>
+        </select>
+
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="default">Default</option>
+          <option value="value">Sort by Value</option>
+        </select>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+        {filteredAndSortedArtists.map((artist) => (
+          <ArtistCard key={artist.artistName} artist={artist} />
+        ))}
+      </div>
+    </>
   );
 }
