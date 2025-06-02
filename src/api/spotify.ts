d0b74fd34e59ce50163633587605aae5e0826a8e
Prue Singh <pruesinghnagra@gmail.com>
2025-06-02 23:16:10 +1200

    new api functions

--- src/api/spotify.ts
+++ src/api/spotify.ts
@@ -16,3 +16,34 @@ export async function fetchSpotifyToken() {
         return null;
     }
 }
+
+export async function fetchWebApi(
+    endpoint: string,
+    method: string,
+    body?: any,
+) {
+    const token = await fetchSpotifyToken();
+    const res = await fetch(`https://api.spotify.com/${endpoint}`, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+        },
+        method,
+        ...(body && { body: JSON.stringify(body) }),
+    });
+
+    if (!res.ok) {
+        const text = await res.text(); // Safely log error response
+        throw new Error(`API error ${res.status}: ${text}`);
+    }
+
+    return res.json();
+}
+
+export async function getArtistsTopTracks(artistId: string) {
+    const data = await fetchWebApi(
+        `v1/artists/${artistId}/top-tracks?market=NZ`,
+        "GET",
+    );
+    return data.tracks;
+}
