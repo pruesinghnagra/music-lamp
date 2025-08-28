264038de0a6a4370e8e57887bfab487d58d5da10
Prue Singh <pruesinghnagra@gmail.com>
2025-08-28 13:42:48 +1200

    Rewrite homepage intro
    - todo: write a homepage route when I am not being lazy

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,11 @@
-// import LocationGraph from '../components/LocationGraph';
-
 export default function Home() {
   return (
     <div className="p-8">
-      <h1 className="text-4xl font-bold mb-4">Welcome to Music Lamp</h1>
       <p className="text-gray-700 mb-4">
-        This site explores independent music scenes through data. Use the navigation above to browse
-        artist stats and learn more about the project.
+        Music Lamp is a place for exploring the music scene of Tamaki Makaurau ~ Auckland. From
+        reviewing albums, spotlighting artists and covering local venues, this is an online zine
+        documenting my adventures in community and music.
       </p>
-      {/* <LocationGraph /> */}
     </div>
   );
 }
