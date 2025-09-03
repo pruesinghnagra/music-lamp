8175c893645b04b5da74dcb049fedd2e7e9dd48f
Prue Singh <pruesinghnagra@gmail.com>
2025-09-03 21:15:58 +1200

    Change to home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 export default function Home() {
   return (
-    <main className="mt-8 px-4 max-w-4xl mx-auto relative overflow-hidden">
-      <p className="text-gray-700 mb-4">
-        Music Lamp is a place for exploring the music scene of Tamaki Makaurau ~ Auckland. From
-        reviewing albums, spotlighting artists and covering local venues, this is an online zine
-        documenting my adventures in community and music.
-      </p>
-    </main>
+    <>
+      <main className="mt-8 px-4 max-w-4xl mx-auto relative overflow-hidden bg-lime-200">
+        <p className="text-6xl tracking-wider">
+          Music Lamp is a place for exploring the music scene of Tamaki Makaurau ~ Auckland. From
+          reviewing albums, spotlighting artists and covering local venues, this is an online zine
+          documenting community and music.
+        </p>
+      </main>
+    </>
   );
 }
